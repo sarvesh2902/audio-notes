2393745418ad6a17a74b10ad47a56d65d376274d
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-12-30 14:22:55 +0530

    added Project Page

--- frontend/components/CreateProjBtn.jsx
+++ frontend/components/CreateProjBtn.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const CreateProjBtn = () => {
   return (
     <div className="flex justify-end mx-3 my-3">
-      <Link href="/">
+      <Link href="/projectPage">
         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <div className="flex flex-row">
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white text-black rounded-md group-hover:bg-opacity-0">
---
+++ frontend/pages/projectPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const projectPage = () => {
+  return (
+    <div className="h-screen flex items-center overflow-hidden">
+      <div className="m-auto px-6 sm:px-0 sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-4/12">
+        <div className="relative group w-full h-64 flex justify-center items-center">
+          <div className="absolute inset-0 w-full h-full rounded-xl bg-white bg-opacity-80 shadow-2xl backdrop-blur-xl group-hover:bg-opacity-70 group-hover:scale-110 transition duration-300"></div>
+          <input
+            accept=".jpg, .jpeg .png, .svg, .webp"
+            className="relative z-10 opacity-0 h-full w-full cursor-pointer"
+            type="file"
+            name="bgfile"
+            id="bgfile"
+          />
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full m-auo flex items-center justify-center">
+            <div className="space-y-6 text-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMfnFzfURm7u65cptRathlu6PJj5Yx9hkxw&usqp=CAU"
+                className="sm:w-40 w-32 m-auto"
+                alt="illustration"
+              />
+              <p className="text-gray-700 text-lg">
+                Drag and drop a file or{" "}
+                <label
+                  for="dragOver"
+                  title="Upload a file"
+                  className="relative z-20 cursor-pointer text-blue-500 hover:text-blue-600 block"
+                >
+                  Upload a file
+                </label>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default projectPage;
