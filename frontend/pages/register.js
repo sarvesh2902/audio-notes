e985f251c16238c95d112fda7208a569b595bc5b
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2022-12-30 14:01:38 +0530

    added login register

---
+++ frontend/pages/register.js
@@ -0,0 +1,89 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import { useState } from "react";
+const register = () => {
+  const [formData, setFormData] = useState({
+    Name:"",
+    Email: "",
+    Password: "",
+   
+
+});
+const handleChange = (event) => {
+  setFormData((prevState) => {
+      return {
+          ...prevState,
+          [event.target.name]: event.target.value,
+      };
+  });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+   setFormData({
+    Name:"",
+    Email: "",
+    Password: "",
+  });
+};
+  return (
+    <div>
+      <Navbar />
+      
+      <section className="bg-gray-50">
+  <div className="flex flex-col items-center px-6  mx-auto md:h-screen lg:py-0 mt-10">
+     
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create and account
+              </h1>
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
+              <div>
+                      <label for="Name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                      <input type="text" 
+                       id="Name" name="Name"
+                       value={formData.Name} 
+                       onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john dev" required />
+                  </div>
+                  <div>
+                      <label for="Email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email"
+                       id="Email" name="Email"
+                       value={formData.Email} 
+                       onChange={handleChange} 
+                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                  </div>
+                  <div>
+                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" 
+                      id="Password" name="Password"
+                      value={formData.Password} 
+                      onChange={handleChange}
+                      placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  </div>
+                 
+                  <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required/>
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label for="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      </div>
+                  </div>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+
+<Footer />
+    </div>
+  )
+}
+
+export default register
