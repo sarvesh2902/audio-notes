ef9624f09b28fdd65f3b9099333af1c302e077ac
Sarvesh Patil <sarvesh2902@gmail.com>
2023-01-07 15:03:26 +0530

    added download audio feature

--- frontend/components/ProjectList.jsx
+++ frontend/components/ProjectList.jsx
@@ -1,66 +1,68 @@
+import Link from "next/link";
 import React from "react";
 
 const ProjectList = () => {
-    return (
-        <div>
-            <div className="overflow-x-auto relative shadow-md sm:rounded-lg mx-20 my-3">
-                <table className="w-full text-sm text-left text-black dark:text-gray-400">
-                    <thead className="text-xs text-white dark:border-gray-400 uppercase bg-primary-200 dark:bg-gray-200 dark:text-black">
-                        <tr>
-                            <th scope="col" className="py-3 px-6">
-                                Project name
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Duration
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Date added
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Date modified
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-200 text-black dark:border-gray-400 hover:bg-gray-50 dark:hover:bg-gray-300">
-                            <th
-                                scope="row"
-                                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-black"
-                            >
-                                Podcast 1
-                            </th>
-                            <td className="py-4 px-6">54 mins </td>
-                            <td className="py-4 px-6">24/02/2022</td>
-                            <td className="py-4 px-6">30/12/2022</td>
-                        </tr>
+  const data = [
+    {
+      name: "project1",
+      duration: "54 mins",
+      dataAdded: "24/02/2022",
+      dateModified: "30/12/2022",
+    },
+    {
+      name: "project2",
+      duration: "54 mins",
+      dataAdded: "24/02/2022",
+      dateModified: "30/12/2022",
+    },
+    {
+      name: "project3",
+      duration: "54 mins",
+      dataAdded: "24/02/2022",
+      dateModified: "30/12/2022",
+    },
+  ];
 
-                        <tr className="bg-white border-b dark:bg-gray-200 text-black dark:border-gray-400 hover:bg-gray-50 dark:hover:bg-gray-300">
-                            <th
-                                scope="row"
-                                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-black"
-                            >
-                                Podcast 2
-                            </th>
-                            <td className="py-4 px-6">54 mins</td>
-                            <td className="py-4 px-6">23/11/2022</td>
-                            <td className="py-4 px-6">05/12/2022</td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-200 text-black dark:border-gray-400 hover:bg-gray-50 dark:hover:bg-gray-300">
-                            <th
-                                scope="row"
-                                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-black"
-                            >
-                                Podcast 3
-                            </th>
-                            <td className="py-4 px-6">39 mins </td>
-                            <td className="py-4 px-6">12/12/2022</td>
-                            <td className="py-4 px-6">29/12/2022</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+  const tableRows = data.map((item) => {
+    return (
+      <tr className="bg-white border-b dark:bg-gray-200 text-black dark:border-gray-400 hover:bg-gray-50 dark:hover:bg-gray-300">
+        <th
+          scope="row"
+          className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-black"
+        >
+          <Link href={item.name}>{item.name}</Link>
+        </th>
+        <td className="py-4 px-6">{item.duration}</td>
+        <td className="py-4 px-6">{item.dataAdded}</td>
+        <td className="py-4 px-6">{item.dateModified}</td>
+      </tr>
     );
+  });
+  return (
+    <div>
+      <div className="overflow-x-auto relative shadow-md sm:rounded-lg mx-20 my-3">
+        <table className="w-full text-sm text-left text-black dark:text-gray-400">
+          <thead className="text-xs text-white dark:border-gray-400 uppercase bg-primary-200 dark:bg-gray-200 dark:text-black">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Project name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Duration
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Date added
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Date modified
+              </th>
+            </tr>
+          </thead>
+          <tbody>{tableRows}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectList;
--- frontend/pages/[project].jsx
+++ frontend/pages/[project].jsx
@@ -6,6 +6,8 @@ import Layout from "../components/Layout";
 import { Button } from "flowbite-react";
 import { useState, useRef } from "react";
 import ShareAudioHandles from "../components/ShareAudioHandles";
+import axios from "axios";
+import fileDownload from "js-file-download";
 // import 'tw-elements'
 // import "./project.scss"
 
@@ -52,6 +54,16 @@ const project = () => {
     setFormData(copy);
   };
 
+  const handleDownload = async (url, filename) => {
+    await axios
+      .get(url, {
+        responseType: "blob",
+      })
+      .then((res) => {
+        fileDownload(res.data, filename);
+      });
+  };
+
   const handlePlay = (timestamp) => {
     console.log(timestamp);
     const timestampSplit = timestamp.split(":");
@@ -86,7 +98,7 @@ const project = () => {
         Project Name
       </h1>
       <div className="flex justify-center my-10">
-        <div className="w-96 ">
+        <div className="w-1/2">
           <AudioPlayer
             ref={audioRef}
             src="https://res.cloudinary.com/sarveshp46/video/upload/v1672472551/Kalimba_ofzs7s.mp3"
@@ -95,7 +107,37 @@ const project = () => {
             // other props here
           />
         </div>
-        <div className="my-auto ml-20">
+      </div>
+      <div className="flex justify-center">
+        <div className="my-auto mx-2">
+          <Button
+            outline={true}
+            gradientDuoTone="cyanToBlue"
+            onClick={() => {
+              handleDownload(
+                "https://res.cloudinary.com/sarveshp46/video/upload/v1672472551/Kalimba_ofzs7s.mp3",
+                "project1.mp3"
+              );
+            }}
+          >
+            Download
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 ml-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </Button>
+        </div>
+        <div className="my-auto mx-2">
           <Button
             outline={true}
             gradientDuoTone="cyanToBlue"
@@ -120,7 +162,7 @@ const project = () => {
         </div>
       </div>
 
-      <div className="inline-flex justify-center items-center w-full">
+      <div className="inline-flex justify-center items-center w-full mt-5">
         <hr className="my-8 w-64 h-px bg-gray-900 border-0 dark:bg-gray-700" />
         <span className="absolute left-1/2 px-3 font-bold text-black text-2xl bg-white -translate-x-1/2 dark:text-white dark:bg-gray-900">
           NOTES
@@ -132,7 +174,7 @@ const project = () => {
         handleDelete={handleDelete}
         handleEdit={handleEdit}
       />
-      <ShareAudioHandles/>
+      <ShareAudioHandles />
     </Layout>
   );
 };
