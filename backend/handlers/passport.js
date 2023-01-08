f401a947cb664e15334a8315be0ac1e9dccb563e
Sarvesh Patil <sarvesh2902@gmail.com>
2023-01-08 12:40:00 +0530

    minor changes

--- backend/handlers/passport.js
+++ backend/handlers/passport.js
@@ -1,53 +1,55 @@
-const LocalStrategy = require('passport-local').Strategy;
-const UserSchema = require('../models/UserSchema');
-const bcryptjs= require('bcryptjs')
+const LocalStrategy = require("passport-local").Strategy;
+const UserSchema = require("../models/UserSchema");
+const bcryptjs = require("bcryptjs");
 
+exports.initializingPassport = (passport) => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      async (username, password, done) => {
+        try {
+          const user = await UserSchema.findOne({ email: username });
 
+          if (!user) return done(null, false);
 
-exports.initializingPassport = (passport)=>{
-  passport.use(new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-  },async (username,password,done)=>{
-      try{
-           const user = await UserSchema.findOne({email:username});
+          // Load hash from your password DB.
+          console.log(await bcryptjs.compare(password, user.password));
 
-           if(!user) return done(null,false);
+          if (!(await bcryptjs.compare(password, user.password)))
+            return done(null, false);
 
-            // Load hash from your password DB.
-            console.log(await bcryptjs.compare(password, user.password));
-
-            if(! await bcryptjs.compare(password, user.password)) return done(null,false);
-
-           return done(null, user);
-      } catch (error) {
+          return done(null, user);
+        } catch (error) {
           return done(error, false);
+        }
       }
-  }
-  ))
-
+    )
+  );
 
   passport.serializeUser((user, done) => {
-      done(null, user.id);
+    done(null, user.id);
   });
 
-  passport.deserializeUser(async (id, done) =>{
-      try{
-          const user = await UserSchema.findById(id);
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await UserSchema.findById(id);
 
-          done(null, user);
-      }catch(error){
-          done(error, false);
-      }
-  })
-}
+      done(null, user);
+    } catch (error) {
+      done(error, false);
+    }
+  });
+};
 
 exports.isAuthenticated = (req, res, next) => {
   if (req.session.user) {
-    return next()
+    return next();
   }
   // res.status(401).redirect('/login')
   res.status(401).json({
-    "message":"Please login first"
-  })
+    message: "Please login first",
+  });
 };
